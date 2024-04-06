4298bf0549d705f42c5454ce2794e3fbb76a7335
Matthew Jankowski <matthew.d.jankowski@ndsu.edu>
2024-04-05 19:03:49 -0500

    updating stuff

--- Projects for Django/locallibrary/catalog/admin.py
+++ Projects for Django/locallibrary/catalog/admin.py
@@ -1,22 +1,46 @@
 from django.contrib import admin
 
-from .models import Author, Genre, Book, BookInstance
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstance, Language
+
 admin.site.register(Book)
 admin.site.register(Author)
-admin.site.register(Genre)
 admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
 
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-    def __str__(self):
-        return self.list_display
+    inlines = [BooksInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-    def __str__(self):
-        return self.list_display
+    inlines = [BooksInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -24,6 +48,6 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
-            'fields': ('status', 'due_back')
+            'fields': ('status', 'due_back', 'borrower')
         }),
     )

0aafb724c12a84a9d24f0a538278c4050a384eee
Matthew Jankowski <matthew.d.jankowski@ndsu.edu>
2024-04-05 19:59:27 -0500

    we're getting there

--- Projects for Django/locallibrary/catalog/admin.py
+++ Projects for Django/locallibrary/catalog/admin.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Author, Genre, Book, BookInstance, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
 
 # Define the admin class
@@ -14,20 +11,14 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-
-
-
-# Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
-@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
+admin.site.register(Book, BookAdmin)
 
 # Register the Admin classes for BookInstance using the decorator
-@admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
 
@@ -39,4 +30,4 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
-
+admin.site.register(BookInstance, BookInstanceAdmin)

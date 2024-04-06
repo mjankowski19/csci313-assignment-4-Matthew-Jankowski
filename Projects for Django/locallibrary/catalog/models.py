0d55da12d5dc3dcea083fbb63facd445e7b1e831
Matthew Jankowski <matthew.d.jankowski@ndsu.edu>
2024-04-05 21:49:16 -0500

    almost there

--- Projects for Django/locallibrary/catalog/models.py
+++ Projects for Django/locallibrary/catalog/models.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
@@ -75,6 +78,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
